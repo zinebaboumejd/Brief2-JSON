9dc756fc037f3009e44b251da7522ec1ffc5daa4
zinebaboumejd <zinebaboumejd@gmail.com>
2022-09-29 14:59:57 +0100

    main js

---
+++ main.js
@@ -0,0 +1,17 @@
+let email =document.getElementById("email");
+let password =document.getElementById("password");
+
+let user = {
+    email: email.value,
+    password: password.value
+};
+// let json = JSON.stringify(user);
+// localStorage.setItem("user", json);
+// console.log("user add");
+
+
+
+function login(){
+
+  
+}
