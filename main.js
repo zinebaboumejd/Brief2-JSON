8f15ae091efb49a937300a299b3312a964980885
zinebaboumejd <zinebaboumejd@gmail.com>
2022-09-29 15:40:41 +0100

    login and signUP

--- main.js
+++ main.js
@@ -27,6 +27,8 @@ function login(){
     let user1=JSON.parse(localStorage.getItem("user"));
     if(user.email==user1.email && user.password==user1.password){
         console.log("login successful");
+        window.location.href="index.html";
+        console.log("---");
     }
     else{
         console.log("login failed");
