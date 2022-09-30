d660506d4a6d98515fb881ac4ea24f4fc52e71e0
nadia <nadiaelhadri99@gmail.com>
2022-09-30 15:10:07 +0100

    modifier main.js ajouter admin.html

--- .history/JS/main_20220930150825.js
+++ .history/JS/main_20220930150825.js
@@ -31,7 +31,7 @@ function signup(){
       if(user.email==user1.email && user.password==user1.password){
           console.log("login successful");
           if(user1.rool==0){
-            window.location.href="../views/home.html";
+            window.location.href="../HTML/home.html";
           }else{
               window.location.href="admin.html";
           }
@@ -43,7 +43,7 @@ function signup(){
       }
   }
   function logout(){
-      window.location.href="../views/home.html";
+      window.location.href="../HTML/home.html";
   }
   
   
