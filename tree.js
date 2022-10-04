e09cb35ec04126562ea6e4afd9bb1c24c68e3b38
zinebaboumejd <zinebaboumejd@gmail.com>
2022-10-04 10:01:49 +0100

    add test

---
+++ tree.js
@@ -0,0 +1,69 @@
+function Click(){
+// json file
+
+const tree = document.getElementById('tree').value;
+
+
+const trackValue = JSON.parse(tree);
+
+const getVal = () => {
+  for (let val in trackValue) {
+    let arr = trackValue[val];
+
+    const objProp = (obj) => {
+      for (let val2 in obj) {
+        if (typeof obj[val2] == "object") {
+          console.log(val2, ": ");
+          objProp(obj[val2]);
+        } else {
+          console.log(val2, ": ", obj[val2]);
+        }
+      }
+    };
+    //afficher les valeurs sur html
+   
+    objProp(arr);
+    // afficher les valeurs de l'objet dans html
+    // creat element
+  
+
+
+
+  }
+};
+getVal();
+// afficher le resultat dans le html arbare
+
+
+
+
+
+
+// json file
+
+// fetch("../treeData.json")
+//     .then((response) => response.json())
+//     .then((data) => {
+//         // console.log(data);
+//         const objProp = (obj) => {
+//             for (let val2 in obj) {
+//               if (typeof obj[val2] == "object") {
+                
+//                   console.log("nested object inside : ", val2);
+//                 objProp(obj[val2]);
+//               } else {
+//                 console.log(val2, ": ", obj[val2]);
+//               }
+//             }
+//           };
+//           objProp(data);
+//     });
+
+}
+    
+
+ 
+
+  
+  
+
